d63e798eff41631bf2b22a4d7d066bbbfe80c1fb
Zbyszko Papierski <zpapierski@wikimedia.org>
2020-06-04 08:28:00 +0200

    Starter TDD project

---
+++ js/bowling_scorer.js
@@ -0,0 +1,11 @@
+class BowlingScorer {
+    roll(pins) {
+
+    }
+
+    score() {
+        return 0;
+    }
+}
+
+module.exports = BowlingScorer
---
+++ test/bowling_scorer.test.js
@@ -0,0 +1,7 @@
+const assert = require('assert');
+const BowlingScorer = require('../js/bowling_scorer')
+describe('Bowling scorer', () => {
+    it('should score 0 in the beginning', () => {
+
+    });
+})
