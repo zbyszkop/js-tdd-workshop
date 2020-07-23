9c60264a6d209b458d40c46e4e1c292c6124bae5
Zbyszko Papierski <zpapierski@wikimedia.org>
2020-07-23 18:02:57 +0200

    next player

--- js/bowling_scorer.js
+++ js/bowling_scorer.js
@@ -1,10 +1,22 @@
 class BowlingScorer {
-    roll(pins) {
 
+    constructor() {
+        this.currentScore = 0;
+        this.throwCount = 0;
+        this.frameScore = 0;
+    }
+    roll(pins) {
+        if (this.throwCount % 2 === 0) {
+            this.frameScore += pins;
+        } else {
+            this.currentScore += this.frameScore + pins;
+            this.frameScore = 0;
+        }
+        this.throwCount++;
     }
 
     score() {
-        return 0;
+        return this.currentScore;
     }
 }
 
