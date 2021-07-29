7834e81da54f524e17fa5685d578a8a27ccef9b2
mewoph <mophaswongse@wikimedia.org>
2021-07-29 11:28:53 -0700

    more tests

--- js/bowling_scorer.js
+++ js/bowling_scorer.js
@@ -4,14 +4,18 @@ class BowlingScorer {
         this.isFirstRoll = true;
         this.lastPins = 0;
         this.tally = 0;
-
+        this.lastSpare = false;
     }
 
     roll(pins) {
         if (!this.isFirstRoll) {
             if (this.lastPins + pins !== 10) {
                 this.tally += this.lastPins + pins;
+            } else {
+                this.lastSpare = true;
             }
+        } else if (this.lastSpare) {
+            this.tally += pins + 10;
         }
         this.isFirstRoll = !this.isFirstRoll;
         this.lastPins = pins;
--- test/bowling_scorer.test.js
+++ test/bowling_scorer.test.js
@@ -27,9 +27,23 @@ describe('Bowling scorer', () => {
 
     });
 
-    it('should score 0 after 2 rolls if a spare in the frame', () => {
+    it('should tally the score after 3 rolls if a spare in the frame', () => {
         bowlingScorer.roll(4)
         bowlingScorer.roll(6)
         assert.equal(bowlingScorer.score(), 0);
+        bowlingScorer.roll(7);
+        assert.equal(bowlingScorer.score(), 10 + 7);
+        bowlingScorer.roll(2);
+        assert.equal(bowlingScorer.score(), 10 + 7 + 9);
+    });
+
+    it('should tally the score after 5 rolls after two spare frames', () => {
+        bowlingScorer.roll(4)
+        bowlingScorer.roll(6)
+        assert.equal(bowlingScorer.score(), 0);
+        bowlingScorer.roll(5)
+        assert.equal(bowlingScorer.score(), 10 + 5);
+        bowlingScorer.roll(5)
+        assert.equal(bowlingScorer.score(), 10 + 5);
     });
 })
