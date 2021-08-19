90ce55862e88a6db0eff5f095f4138d0136b8576
Gabriel Pita <dvpita@gmail.com>
2021-08-19 11:24:43 +0200

    tally after spare

--- js/bowling_scorer.js
+++ js/bowling_scorer.js
@@ -3,15 +3,25 @@ class BowlingScorer {
         this.isFirstRoll = true;
         this.lastRoll = 0;
         this.currentScore = 0;
+        this.spare = false;
     }
 
     roll(pins) {
         if (this.isFirstRoll) {
+            if(this.spare){
+                this.currentScore+=10+pins
+                this.spare=false
+            }
+                
             this.lastRoll = pins;
             this.isFirstRoll = false;
         } else {
-            this.currentScore += this.lastRoll + pins;
+            if(this.lastRoll+pins!=10)
+                this.currentScore += this.lastRoll + pins;
+            else
+                this.spare=true
             this.isFirstRoll = true;
+
         }
     }
 
--- test/bowling_scorer.test.js
+++ test/bowling_scorer.test.js
@@ -3,22 +3,55 @@ const BowlingScorer = require('../js/bowling_scorer')
 describe('Bowling scorer', () => {
     it('should score a regular frame', () => {
         let bowlingScorer = new BowlingScorer();
-        assert.equal(bowlingScorer.score(), 0);
+        assert.strictEqual(bowlingScorer.score(), 0);
         bowlingScorer.roll(4)
-        assert.equal(bowlingScorer.score(), 0);
+        assert.strictEqual(bowlingScorer.score(), 0);
         bowlingScorer.roll(3)
-        assert.equal(bowlingScorer.score(), 7);
+        assert.strictEqual(bowlingScorer.score(), 7);
     });
 
     it('should score 2 frames', () => {
         let bowlingScorer = new BowlingScorer();
-        assert.equal(bowlingScorer.score(), 0);
+        assert.strictEqual(bowlingScorer.score(), 0);
         bowlingScorer.roll(4)    // 1st roll, 1st frame
-        assert.equal(bowlingScorer.score(), 0);
+        assert.strictEqual(bowlingScorer.score(), 0);
         bowlingScorer.roll(3)   // 2nd roll, 1st frame, tally
-        assert.equal(bowlingScorer.score(), 7);
+        assert.strictEqual(bowlingScorer.score(), 7);
         bowlingScorer.roll(2)  // 1st roll, 2nd frame
-        assert.equal(bowlingScorer.score(), 7);
-
+        assert.strictEqual(bowlingScorer.score(), 7);
+        bowlingScorer.roll(7)  // 2nd roll, 2nd frame
+        assert.strictEqual(bowlingScorer.score(), 16);
     });
+
+    it('should score a spare', () => {
+        let bowlingScorer = new BowlingScorer();
+        assert.strictEqual(bowlingScorer.score(), 0);
+        bowlingScorer.roll(4)    // 1st roll, 1st frame
+        assert.strictEqual(bowlingScorer.score(), 0);
+        bowlingScorer.roll(6)   // 2nd roll, 1st frame, tally
+        assert.strictEqual(bowlingScorer.score(), 0);
+        bowlingScorer.roll(2)  // 1st roll, 2nd frame
+        assert.strictEqual(bowlingScorer.score(), 12);
+        bowlingScorer.roll(7) // 2nd roll, 2nd frame
+        assert.strictEqual(bowlingScorer.score(), 21)
+    });
+
+    it('should score a spare and a regular frame after', () => {
+        let bowlingScorer = new BowlingScorer();
+        assert.strictEqual(bowlingScorer.score(), 0);
+        bowlingScorer.roll(4)    // 1st roll, 1st frame
+        assert.strictEqual(bowlingScorer.score(), 0);
+        bowlingScorer.roll(6)   // 2nd roll, 1st frame, tally
+        assert.strictEqual(bowlingScorer.score(), 0);
+        bowlingScorer.roll(2)  // 1st roll, 2nd frame
+        assert.strictEqual(bowlingScorer.score(), 12);
+        bowlingScorer.roll(7) // 2nd roll, 2nd frame
+        assert.strictEqual(bowlingScorer.score(), 21)
+        bowlingScorer.roll(2)  // 1st roll, 3rd frame
+        assert.strictEqual(bowlingScorer.score(), 21);
+        bowlingScorer.roll(7) // 2nd roll, 3rd frame
+        assert.strictEqual(bowlingScorer.score(), 30)
+    });
+
+    
 })
