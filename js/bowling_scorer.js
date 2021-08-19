057e2d5b6f0ef176b6061ae554e2b8ceab442a62
Zbyszko Papierski <zpapierski@wikimedia.org>
2021-08-19 10:48:16 +0200

    First rule implemented

--- js/bowling_scorer.js
+++ js/bowling_scorer.js
@@ -1,11 +1,24 @@
 class BowlingScorer {
-    roll(pins) {
+    constructor() {
+        this.isFirstRoll = true;
+        this.lastRoll = 0;
+        this.currentScore = 0;
+    }
 
+    roll(pins) {
+        if (this.isFirstRoll) {
+            this.lastRoll = pins;
+            this.isFirstRoll = false;
+        } else {
+            this.currentScore += this.lastRoll + pins;
+            this.isFirstRoll = true;
+        }
     }
 
     score() {
-        return 0;
+        return this.currentScore;
     }
 }
 
 module.exports = BowlingScorer
+
--- test/bowling_scorer.test.js
+++ test/bowling_scorer.test.js
@@ -1,7 +1,24 @@
 const assert = require('assert');
 const BowlingScorer = require('../js/bowling_scorer')
 describe('Bowling scorer', () => {
-    it('should score 0 in the beginning', () => {
+    it('should score a regular frame', () => {
+        let bowlingScorer = new BowlingScorer();
+        assert.equal(bowlingScorer.score(), 0);
+        bowlingScorer.roll(4)
+        assert.equal(bowlingScorer.score(), 0);
+        bowlingScorer.roll(3)
+        assert.equal(bowlingScorer.score(), 7);
+    });
+
+    it('should score 2 frames', () => {
+        let bowlingScorer = new BowlingScorer();
+        assert.equal(bowlingScorer.score(), 0);
+        bowlingScorer.roll(4)    // 1st roll, 1st frame
+        assert.equal(bowlingScorer.score(), 0);
+        bowlingScorer.roll(3)   // 2nd roll, 1st frame, tally
+        assert.equal(bowlingScorer.score(), 7);
+        bowlingScorer.roll(2)  // 1st roll, 2nd frame
+        assert.equal(bowlingScorer.score(), 7);
 
     });
 })
