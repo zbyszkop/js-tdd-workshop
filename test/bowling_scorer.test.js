2b0a44a2a69d909530cf51b57ad3c60c55f63910
Zbyszko Papierski <zpapierski@wikimedia.org>
2021-12-02 10:23:21 +0100

    First rule test

--- test/bowling_scorer.test.js
+++ test/bowling_scorer.test.js
@@ -2,6 +2,15 @@ const assert = require('assert');
 const BowlingScorer = require('../js/bowling_scorer')
 describe('Bowling scorer', () => {
     it('should score 0 in the beginning', () => {
-
+        const bowlingScorer = new BowlingScorer();
+        assert.equal(bowlingScorer.score(), 0);
     });
+
+    it('should tally up points only after a complete frame', () => {
+        const bowlingScorer = new BowlingScorer();
+        bowlingScorer.roll(3);
+        assert.equal(bowlingScorer.score(), 0);
+        bowlingScorer.roll(4);
+        assert.equal(bowlingScorer.score(), 7);
+    })
 })
